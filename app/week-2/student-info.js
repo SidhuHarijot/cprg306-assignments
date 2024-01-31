10bc98c169ff1f6fe445f27c9511bcccbdde8376
Harijot Singh <125294510+SidhuHarijot@users.noreply.github.com>
2024-01-30 23:02:49 -0700

    Completed Week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+export default function StudentInfo(){
+    return (
+        <div>
+        <h2>
+            Name: Harijot Singh
+        </h2>
+        <p>
+            github: https://github.com/SidhuHarijot
+        </p>
+        </div>
+    )
+}
